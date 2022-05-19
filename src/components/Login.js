fc66ee84816c460bc90f9946e245673ab37ea905
Your Name <you@example.com>
2022-05-19 06:40:33 +0600

    almost done

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,41 @@
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import auth from '../../firebase.init';
+
+
 
 const Login = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+    let navigate = useNavigate();
+    let location = useLocation();
+  
+    let from = location.state?.from?.pathname || "/";
+
+    if(user){
+        navigate(from, {replace:true});
+    }
+  
+
+    if(loading){
+        return <p>Loading.......</p>
+    }
+
+    if(error){
+        toast.error(error)
+    }
+
+
     return (
-        <div>
+        <div className='w-full py-2 flex justify-center items-center h-screen'>
+            <div>
+                 <button 
+                    onClick={() => signInWithGoogle()}
+                    className='btn'>Sign In With Google</button> :
+                
+            </div>
             
         </div>
     );
--- src/components/ToDoTaskList.js
+++ src/components/ToDoTaskList.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
-const ToDoTaskList = () => {
-    return (
-        <div>
-            
+const ToDoTaskList = ({ items , handleDelete}) => {
+
+  
+  return (
+    <div className="w-full ">
+      {items?.map((item) => (
+        <div
+          className="flex justify-between items-center w-[400px] p-4 border border-rose-400 text-black rounded mx-auto my-4 "
+          key={item?._id}
+        >
+          <h1>{item?.name}</h1>
+          <p>{item?.description}</p>
+          <button onClick={() => handleDelete(item?._id)} className="btn btn-xs ">Delete</button>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default ToDoTaskList;
